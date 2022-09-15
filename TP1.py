e6ed7bfbb8dd9d331c16d436586c5b0bda3c7b5f
mayervi <mayervi@educ.college-montreal.qc.ca>
2022-09-15 08:46:00 -0400

    TP1

---
+++ TP1.py
@@ -0,0 +1,7 @@
+#nombre de mots#
+txt = "je m'appel vincent mayer"
+motsdanslaphrase_list = txt.split()
+mots_dans_la_phrase = len(motsdanslaphrase_list)
+print("Le nombre de mots est affiché ici")
+print(mots_dans_la_phrase)
+
